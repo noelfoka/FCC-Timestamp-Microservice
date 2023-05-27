390657cbe03a16969b05f25a8d7f63d20eb6b881
Replit user <>
2023-05-27 06:59:07 +0000

    first project completed

--- index.js
+++ index.js
@@ -20,10 +20,33 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+const isInvalidDate = (date) => date.toUTCString() === 'Invalid Date';
+
+app.get('/api/:date', (req, res) => {
+  let date = new Date(req.params.date);
+
+  if(isInvalidDate(date)) {
+    date = new Date(+req.params.date);
+  }
+
+  if(isInvalidDate(date)) {
+    res.json({ error: 'invalid Date' });
+    return;
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
+app.get('/api', (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  });
+});
 
 
 // listen for requests :)
